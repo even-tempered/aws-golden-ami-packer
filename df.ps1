066e6291c2c09bc0762399afc6a5f6d5896ae014
Saurabh Deshmukh <151554664+even-tempered@users.noreply.github.com>
2024-05-07 15:25:23 +0530

    Delete df.ps1

--- df.ps1
+++
@@ -1,7 +0,0 @@
-# Set default password
-$adminPassword = "Password@123"
-$secureAdminPassword = ConvertTo-SecureString -String $adminPassword -AsPlainText -Force
-net user Administrator $adminPassword
- 
-# Run sysprep
-C:\Windows\System32\sysprep\sysprep.exe /generalize /oobe /shutdown /quiet
